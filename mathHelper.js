66692fb63bb656496db111b72bd09ebed814bef4
Victoria Rasavanh <rasavanh.victoria@gmail.com>
2016-04-05 18:27:08 -0500

    Fixed login button redirect
    - also popped in some cookie functions

--- mathHelper.js
+++ mathHelper.js
@@ -198,4 +198,31 @@ $('#submitBtn').click( function() {
 function summaryContent() {
     
 }
+    
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+$('#nameBtn').click( function() {
+    var name = document.getElementById("nameInput").value;
+    //setCookie("NAME",name,1);
+    setNAME(name);
+    alert(NAME);
+    window.location.assign("game.html");
+});
+    
 });
